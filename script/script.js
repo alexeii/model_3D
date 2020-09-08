3c9466820c644943ba2c1b3c248398c24f5bf469
Alexey Arhipiv <alex3171@list.ru>
2020-09-08 12:29:09 +0500

    feat:Home work

--- script/script.js
+++ script/script.js
@@ -269,4 +269,46 @@ window.addEventListener("DOMContentLoaded", () => {
       item.value = item.value.replace(/[^\d]/g, "");
     });
   });
+
+  //калькулятор
+  const calc = (price = 100) => {
+    const calcType = document.querySelector(".calc-type");
+    const calcSquare = document.querySelector(".calc-square");
+    const calcCount = document.querySelector(".calc-count");
+    const calcDay = document.querySelector(".calc-day");
+    const totalValue = document.getElementById("total");
+
+    const countSum = () => {
+      let total = 0;
+      let countValue = 1;
+      let dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value;
+      const squareValue = +calcSquare.value;
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener("change", (event) => {
+      const target = event.target;
+      if (
+        target === calcType ||
+        target === calcSquare ||
+        target === calcCount ||
+        target === calcDay
+      ) {
+        countSum();
+      }
+    });
+  };
+  calc(100);
 });
